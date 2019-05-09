22df00567152726f408dfbd87c3d90bb098cb0ab
YarosJ <jaroslawjuk@gmail.com>
2019-05-09 10:01:13 +0300

    Fixed environment variables for seed DB on server :bug:

--- config/seedDB/index.js
+++ config/seedDB/index.js
@@ -4,7 +4,9 @@ import failures from './failures';
 
 export default async () => {
   const { permanentSeed } = process.env;
-  if (process.env.seedAccessControl) accessControl(permanentSeed);
-  if (process.env.seedAdmin) admin(permanentSeed);
-  if (process.env.seedFailures) failures();
+  const isPermanent = permanentSeed === 'true';
+
+  if (process.env.seedAccessControl === 'true') accessControl(isPermanent);
+  if (process.env.seedAdmin === 'true') admin(isPermanent);
+  if (process.env.seedFailures === 'true') failures();
 };
